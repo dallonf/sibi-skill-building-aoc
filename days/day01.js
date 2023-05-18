017ea7149453d1bf7bf67b81811b53337e7a23e1
Dallon Feldner <dallon@sibipro.com>
2023-05-18 17:16:01 -0500

    Day 1 Part Two

--- days/day01.js
+++ days/day01.js
@@ -29,17 +29,28 @@ const PUZZLE_INPUT = readTextFile("days/day01_puzzleinput.txt");
  */
 function parseInput(input) {
   const lines = splitLines(input);
-  const result = [];
+  /** @type {number[]} */
   let currentElf = [];
+  const result = [currentElf];
   for (const line of lines) {
     if (line === "") {
-      result.push(currentElf);
       currentElf = [];
+      result.push(currentElf);
     } else {
       currentElf.push(parseInt(line, 10));
     }
   }
   return result;
+}
+
+/**
+ * Get number of calories for each elf.
+ *
+ * @param {number[][]} elves
+ * @returns {number[]}
+ */
+function getCaloriesPerElf(elves) {
+  return elves.map((elf) => elf.reduce((acc, curr) => acc + curr, 0));
 }
 
 /**
@@ -50,9 +61,7 @@ function parseInput(input) {
  */
 function getMaxCalories(elves) {
   /** @type {number[]} */
-  const totalCaloriesPerElf = elves.map((elf) =>
-    elf.reduce((acc, curr) => acc + curr, 0)
-  );
+  const totalCaloriesPerElf = getCaloriesPerElf(elves);
   return Math.max(...totalCaloriesPerElf);
 }
 
@@ -65,3 +74,33 @@ assertEquals(
 const part1Answer = getMaxCalories(parseInput(PUZZLE_INPUT));
 console.log("Part 1 answer:", part1Answer);
 assertEquals(part1Answer, 72511, "Part 1");
+
+/**
+ * Returns the N elves that are carrying the most calories, and the total calories each one is carrying.
+ *
+ * @param {number[][]} elves
+ * @param {number} n
+ * @returns {number[]}
+ */
+function getTopNElves(elves, n) {
+  const totalCaloriesPerElf = getCaloriesPerElf(elves);
+  const sortedCalories = totalCaloriesPerElf.sort((a, b) => b - a);
+  const topN = sortedCalories.slice(0, n);
+  return topN;
+}
+
+/**
+ * @param {string} input
+ * @returns {number}
+ */
+function part2(input) {
+  const elves = parseInput(input);
+  const top3 = getTopNElves(elves, 3);
+  return top3.reduce((acc, curr) => acc + curr, 0);
+}
+
+assertEquals(45000, part2(TEST_INPUT), "Part 2, test input");
+
+const part2Answer = part2(PUZZLE_INPUT);
+console.log("Part 2 answer:", part2Answer);
+assertEquals(part2Answer, 212117, "Part 2");
