6ec9f5542ccb2772a58ddcc33f07d092590bc037
Dallon Feldner <dallon@sibipro.com>
2023-05-25 17:28:57 -0500

    Improve assertion framework

--- utils/assert.js
+++ utils/assert.js
@@ -11,17 +11,37 @@ export function assertTrue(condition, message) {
   }
 }
 
+/** @typedef {string | number | boolean | null | undefined} Scalar */
+/** @typedef {Scalar | { [key: string]: SerializableValue } | { [index: number]: SerializableValue } } SerializableValue */
+
 /**
- * @template T
+ * @template {Scalar} T
  *
  * @param {T} actual
  * @param {T} expected
  * @param {string} [message]
  */
 export function assertEquals(actual, expected, message) {
-  let assertMessage = `Expected ${expected} to equal ${actual}; actually was ${actual}`;
+  let assertMessage = `Expected ${actual} to equal ${expected}`;
   if (message) {
     assertMessage = `${message}. ${assertMessage}`;
   }
   assertTrue(expected === actual, assertMessage);
+}
+
+/**
+ * @template {SerializableValue} T
+ *
+ * @param {T} actual
+ * @param {T} expected
+ * @param {string} [message]
+ */
+export function assertDeepEquals(actual, expected, message) {
+  const expectedJson = JSON.stringify(expected, null, 2);
+  const actualJson = JSON.stringify(actual, null, 2);
+  let assertMessage = `Expected ${actualJson} to deep equal ${expectedJson}`;
+  if (message) {
+    assertMessage = `${message}. ${assertMessage}`;
+  }
+  assertTrue(expectedJson === actualJson, assertMessage);
 }
