9d8905ae066675093d5683530441f96212b12f53
Dallon Feldner <dallon@sibipro.com>
2023-06-15 17:49:27 -0500

    Day 5 Part One

--- days/day05.js
+++ days/day05.js
@@ -21,7 +21,13 @@ import { readTextFile, splitLines } from "../utils/files.js";
  */
 
 const TEST_INPUT = readTextFile("days/day05_testinput.txt");
+const PUZZLE_INPUT = readTextFile("days/day05_puzzleinput.txt");
 
+// Notice how many assumptions I'm making here.
+// If this were real user input, I couldn't get away with that! This
+// is expecting a very specific format that a human being probably wouldn't get right
+// all the time, and they would need helpful error messages to figure out what they did wrong.
+// Plus if the input is wrong, it'll at best result in junk data.
 /**
  * @param {string} input
  * @returns {Input}
@@ -102,3 +108,97 @@ assertDeepEquals(TEST_INPUT_PARSED, {
     },
   ],
 });
+
+/**
+ * @param {ContainerStacks} stacks
+ * @param {number} from
+ * @param {number} to
+ * @returns {ContainerStacks}
+ */
+function moveContainer(stacks, from, to) {
+  const fromIndex = from - 1;
+  const toIndex = to - 1;
+  const movedContainer = stacks[fromIndex].at(-1);
+  if (!movedContainer) {
+    throw new Error(`No container to move from stack ${from}`);
+  }
+  return stacks.map((stack, index) => {
+    if (index === fromIndex) {
+      return stack.slice(0, -1);
+    }
+    if (index === toIndex) {
+      return [...stack, movedContainer];
+    }
+    return stack;
+  });
+}
+assertDeepEquals(moveContainer(TEST_INPUT_PARSED.stacks, 2, 1), [
+  ["Z", "N", "D"],
+  ["M", "C"],
+  ["P"],
+]);
+
+/**
+ * @param {ContainerStacks} stacks
+ * @param {Instruction} instruction
+ * @returns {ContainerStacks}
+ */
+function followInstruction(stacks, instruction) {
+  const { numberToMove, from, to } = instruction;
+  let result = stacks;
+  for (let i = 0; i < numberToMove; i += 1) {
+    result = moveContainer(result, from, to);
+  }
+  return result;
+}
+
+/**
+ * @param {ContainerStacks} stacks
+ * @param {Instruction[]} instructions
+ * @returns {ContainerStacks}
+ */
+function followInstructions(stacks, instructions) {
+  return instructions.reduce(followInstruction, stacks);
+}
+
+assertDeepEquals(
+  followInstructions(TEST_INPUT_PARSED.stacks, TEST_INPUT_PARSED.instructions),
+  [["C"], ["M"], ["P", "D", "N", "Z"]]
+);
+
+/**
+ *
+ * @param {ContainerStacks} stacks
+ * @returns {string}
+ */
+function getTopContainersString(stacks) {
+  return stacks
+    .map((stack) => stack.at(-1))
+    .map((container) => container ?? "")
+    .join("");
+}
+
+assertDeepEquals(
+  getTopContainersString(
+    followInstructions(TEST_INPUT_PARSED.stacks, TEST_INPUT_PARSED.instructions)
+  ),
+  "CMZ"
+);
+
+/**
+ * @param {string} input
+ * @returns {string}
+ */
+function partOne(input) {
+  const parsedInput = parseInput(input);
+  const result = getTopContainersString(
+    followInstructions(parsedInput.stacks, parsedInput.instructions)
+  );
+  return result;
+}
+
+assertEquals(partOne(TEST_INPUT), "CMZ");
+
+const partOneAnswer = partOne(PUZZLE_INPUT);
+console.log("Part one:", partOneAnswer);
+assertEquals(partOneAnswer, "FWNSHLDNZ");
